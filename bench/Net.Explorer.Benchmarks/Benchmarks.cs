ea9d8ca06047c9424ca926a572db6118c3314957
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-01-08 15:52:58 +0100

    feat: benchmarking project

---
+++ bench/Net.Explorer.Benchmarks/Benchmarks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace Net.Explorer.Benchmarks
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("World");
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            Console.WriteLine("Hello\nWorld");
+        }
+    }
+}
