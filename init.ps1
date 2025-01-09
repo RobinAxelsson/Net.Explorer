b8f22e761e3789291aa8497b340d6f3f8769ad95
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-01-09 14:04:34 +0100

    feat: add watch

--- init.ps1
+++ init.ps1
@@ -6,3 +6,7 @@ $REP_ROOT=$PSScriptRoot
 Set-Alias build-il $REP_ROOT\src\IL\build-il.bat
 Set-Alias build-masm $REP_ROOT\src\Masm\build-masm.bat
 Set-Alias run-hallo.bench $REP_ROOT\bench\Hallo.Bench\build.bat
+
+function watch-il($file){
+    watch-path "$REP_ROOT\src\IL" "$file.il" "$REP_ROOT\src\IL\build-il.bat $file"
+}
